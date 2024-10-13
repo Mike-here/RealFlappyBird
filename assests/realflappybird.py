8f08785accae27a3934b2dc421b82e7b62246e2d
Michael <macheampong287@gmail.com>
2024-10-12 22:44:06 -0400

    Changes in the size of the background image

--- assests/realflappybird.py
+++ assests/realflappybird.py
@@ -3,21 +3,26 @@ import pygame
 pygame.init()
 
 #screen sizes
-screen_width = 864
-screen_height = 536
+screen_width = 384
+screen_height = 583
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("The Real Flappy Bird")
 
 #load images
-base = pygame.image.load('images/base.png')
+background = pygame.image.load("background-day.png")
 
 # loop
 run = True
 while run:
+
+    screen.blit(background, (0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()        
 
 
 pygame.quit()            
