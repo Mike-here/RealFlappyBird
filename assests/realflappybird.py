09fe4f4180f20c94f693178f2e756be0731b131d
Michael <macheampong287@gmail.com>
2024-10-12 22:25:50 -0400

    Second commit

--- assests/realflappybird.py
+++ assests/realflappybird.py
@@ -1,22 +1,23 @@
 import pygame
-from pygame.locals import *
 
 pygame.init()
 
 #screen sizes
 screen_width = 864
-screen_height = 936
+screen_height = 536
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("The Real Flappy Bird")
 
+#load images
+base = pygame.image.load('images/base.png')
 
 # loop
 run = True
 while run:
-    for event in pygame.get():
-        if event.type == pygame.QUIT():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             run = False
 
-            
+
 pygame.quit()            
